86fb09d365da807cdb3324b76e73d8028685cba1
Nico Froehlich <nicofroehlich@icloud.com>
2022-06-17 13:00:37 +0200

    Update staging buffer

--- Src/scene/GLTFLoader.cpp
+++ Src/scene/GLTFLoader.cpp
@@ -2,6 +2,8 @@
 
 #include <tiny_gltf.h>
 
+#include <MemoryHelper.hpp>
+
 constexpr auto POSITION = "POSITION";
 constexpr auto NORMAL = "NORMAL";
 constexpr auto COLOR = "COLOR";
@@ -19,14 +21,14 @@ constexpr auto enforceSemicolon = 0;
   }                                             \
   enforceSemicolon
 #else
-#define DEBUG_CHECK(assertion, output, exitcon)
+#define DEBUG_CHECK(assertion, output, exitcon) enforceSemicolon
 #endif  // DEBUG
 
 namespace cpr::loader {
 
 std::shared_ptr<Model> GLTFLoader::loadModel(
-    const std::string& modelFile) const {
-  const auto model = std::make_unique<Model>(this->device);
+    const std::string &modelFile) const {
+  auto model = std::make_unique<Model>(this->device);
   std::vector<Vertex> vertex;
   std::vector<unsigned int> indicies;
   std::vector<unsigned int> materialIds;
@@ -65,23 +67,71 @@ std::shared_ptr<Model> GLTFLoader::loadModel(
   DEBUG_CHECK(scene.nodes.empty(),
               "TinyGLTF: No nodes in Scene[" << scene.name << "]!");
 
+#pragma region buffer
+  std::vector<VkBuffer> bufferCreateInfos;
+  bufferCreateInfos.reserve(tglModel.buffers.size());
+
+  VkMemoryAllocateInfo allocateInfo{};
+  allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+
+  uint32_t typeBit{};
+  std::vector<size_t> bufferOffsets;
+  bufferOffsets.reserve(tglModel.buffers.size());
+  for (const auto &buffer : tglModel.buffers) {
+    VkBufferCreateInfo bufferCreateInfo{};
+    bufferCreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    bufferCreateInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+    bufferCreateInfo.size = buffer.data.size();
+
+    auto vkdevice = device->getLogicalDevice();
+
+    VkBuffer vkbuffer;
+    vkCreateBuffer(vkdevice, &bufferCreateInfo, nullptr, &vkbuffer);
+    bufferCreateInfos.push_back(vkbuffer);
+
+    VkMemoryRequirements requirements;
+    vkGetBufferMemoryRequirements(vkdevice, vkbuffer, &requirements);
+    typeBit = requirements.memoryTypeBits;
+    bufferOffsets.push_back(allocateInfo.allocationSize); 
+    requirements.size + requirements.size % requirements.alignment;
+  }
+  allocateInfo.memoryTypeIndex =
+      find_memory_type_index(device->getPhysicalDevice(), typeBit,
+                             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
+
+#pragma endregion
+
+#pragma region node parsing
   for (const auto nodeID : scene.nodes) {
     DEBUG_CHECK(nodeID < 0,
                 "TinyGLTF: Invalid node id in Scene[" << scene.name << "]!",
                 continue);
-    const auto node = tglModel.nodes[nodeID];
+    const auto &node = tglModel.nodes[nodeID];
     if (node.mesh < 0) {
       continue;
     }
-    const auto mesh = tglModel.meshes[node.mesh];
-    for (const auto primitiv : mesh.primitives) {
+
+    const auto &mesh = tglModel.meshes[node.mesh];
+    for (const auto &primitiv : mesh.primitives) {
       const auto primitivEnd = primitiv.attributes.cend();
-      const auto iterator = primitiv.attributes.find(POSITION);
-      if (iterator != primitivEnd) {
-        
+      const auto positionItr = primitiv.attributes.find(POSITION);
+      if (positionItr != primitivEnd) {
+        const auto accessorID = positionItr->second;
+        DEBUG_CHECK(acessorID < 0, "TinyGLTF: Invalid accessor in primitv!",
+                    return nullptr);
+        const auto &accessor = tglModel.accessors[accessorID];
+      }
+      const auto colorItr = primitiv.attributes.find(COLOR);
+      if (colorItr != primitivEnd) {
+        const auto accessorID = colorItr->second;
+        DEBUG_CHECK(acessorID < 0, "TinyGLTF: Invalid accessor in primitv!",
+                    return nullptr);
+        const auto &accessor = tglModel.accessors[accessorID];
       }
     }
   }
+#pragma endregion
 
   model->add_new_mesh(device, transfer_queue, command_pool, vertex, indicies,
                       materialIds, material);
